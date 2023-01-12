fa18efbe88b2b72aa8ae3c0049fd76dbe54f8d7b
madinasagatova <91742360+madinasagatova@users.noreply.github.com>
2023-01-12 22:07:18 +0000

    app.set with path and a new folder views

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ app.get('/', (req, res) =>{
     res.send(`
         <h2>Welcome to Student Database!</h2>
         <h3>Click here to get access to the<b> <a href="/student/list">Database</a></b></h3>
-    `)
+    `);
 
-})
+});
 
+app.set('views', path.join(__dirname, '/views/'))
+
+app.engine
 app.listen(3000, () =>{
     console.log('Server started at port 3000');
 });
