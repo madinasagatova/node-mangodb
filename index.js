641c4950e5171880d853eb402aabbfadac36a23d
madinasagatova <91742360+madinasagatova@users.noreply.github.com>
2023-01-12 22:03:08 +0000

    get api

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const bodyparser = require('body-parser');
 
 var app =express();
 
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+app.get('/', (req, res) =>{
+    res.send(`
+        <h2>Welcome to Student Database!</h2>
+        <h3>Click here to get access to the<b> <a href="/student/list">Database</a></b></h3>
+    `)
+
+})
+
 app.listen(3000, () =>{
     console.log('Server started at port 3000');
 });
