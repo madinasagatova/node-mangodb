a778ac3427bcbf0fcc59221dd7882a22fab94f7e
madinasagatova <91742360+madinasagatova@users.noreply.github.com>
2023-01-15 16:34:09 +0000

    new function insertrecord

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -7,4 +7,25 @@ router.get('/', (req, res) =>{
     res.render('student/addOrEdit', {
         viewTitle: 'Insert Student'
     })
-})
+});
+
+router.post('/', (req, res) =>{
+    if(req.body._id == ''){
+        insertRecord(req, res)
+    } else {
+        updateRecord(req, res)
+    }
+});
+
+function insertRecord(req, res){
+    var student = new Student();
+    student.fullName = req.body.fullName;
+    student.email = req.body.email;
+    student.mobile = req.body.mobile;
+    student.city = req.body.city;
+    student.save((err, doc) => {
+        if(!err) {
+            res.redirect('student/list');
+        }
+    })
+};
